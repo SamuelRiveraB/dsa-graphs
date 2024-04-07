55bb1a6fea74dd2e92854f3842fe4ce47d24b9c1
Samuel <samuelrivba@gmail.com>
2024-04-06 21:22:34 -0500

    Add types of graphs with examples

---
+++ graphs.js
@@ -0,0 +1,18 @@
+// Edge List states the connections
+const graph = [
+  [0, 2],
+  [2, 3],
+  [2, 1],
+  [1, 3],
+];
+
+// Adjacent List states the nodes the index is connected to
+const graph2 = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// Adjacent matrix uses 1 to represent the connections of each node
+const graph3 = [
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0],
+];

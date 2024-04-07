674cef2d0b0ff3ad3dda597e21904f554017a753
Samuel <samuelrivba@gmail.com>
2024-04-06 21:36:40 -0500

    Add graph implementation (addVertex and addEdge)

--- graphs.js
+++ graphs.js
@@ -22,11 +22,28 @@ class Graph {
     this.numberOfNodes = 0;
     this.adjacentList = {};
   }
-  addVertex(node) {}
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+  }
 
-  addEdge(node1, node2) {}
+  addEdge(node1, node2) {
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
+  }
 
-  showConnections() {}
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = "";
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      }
+      console.log(node + "-->" + connections);
+    }
+  }
 }
 
 const myGraph = new Graph();
